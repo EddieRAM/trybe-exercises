93b6d4aba804ba2c4ad2c05d618fcf589bbd372d
EddieRAM <edusacarneiro@gmail.com>
2021-05-19 19:01:56 -0300

    Desafios-das-video-aulas

---
+++ 4.1/switch-case.js
@@ -0,0 +1,16 @@
+let candidato = "reprovado";
+
+switch (candidato) {
+  case "aprovado":
+    console.log("Parabéns!");
+    break;
+  case "lista":
+    console.log("Boa Sorte!");
+    break;
+  case "reprovado":
+    console.log("Se F#!");
+    break;
+
+  default:
+    console.log("Sei não")
+}
