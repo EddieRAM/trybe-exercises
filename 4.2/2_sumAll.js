7616c5002ae7925bf5b3cfda825620ba05d8b27c
EddieRAM <edusacarneiro@gmail.com>
2021-05-20 18:50:23 -0300

    Exercicio 4.2.2 - 2_sumAll

---
+++ 4.2/2_sumAll.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumAll = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sumAll += numbers[index];
+  console.log(sumAll)
+}
