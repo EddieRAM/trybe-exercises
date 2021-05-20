de9dbcc2ab549ea576d814e0cfb946f0b228e785
EddieRAM <edusacarneiro@gmail.com>
2021-05-20 18:03:07 -0300

    Exercicio 4.1.1 - 1_allValues

---
+++ 4.2/1_allValues.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index +=1) {
+  console.log(numbers[index]);
+}
+
+
+
+
+//to log number x length:
+//for (let number of numbers) {
+//  console.log(numbers)
+//}
