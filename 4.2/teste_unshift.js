2fd752267943038f231f767715e50e427d5d53c8
EddieRAM <edusacarneiro@gmail.com>
2021-05-20 17:46:15 -0300

    Testes e Desafios do Course 4.2

---
+++ 4.2/teste_unshift.js
@@ -0,0 +1,6 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
