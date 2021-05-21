9548d0bac63097248960d832850886719bc2adc6
EddieRAM <edusacarneiro@gmail.com>
2021-05-21 18:53:51 -0300

    Exercicio 4.3.2 - 2_printTriangle

---
+++ 4.3/2_printTriangle.js
@@ -0,0 +1,16 @@
+//O código deverá imprimir um asterísco uma vez na primeira linha;
+//Duas vezes na segunda, três na terceira... até a 5 linha.
+
+let printTriangle = [];
+
+for (let index = 0; index < 5; index += 1) {
+  printTriangle.push("*");
+  console.log(printTriangle);  
+}
+
+
+
+// A array "printTriangle" vazia é criada para receber "*";
+// Variável "index" recebe valor de 0 a 5;
+// Loop empurra "*" na variável vazia, 1 de cada vez, até 5;
+// A cada loop, "console.log", dentro do "for", imprime a array.
