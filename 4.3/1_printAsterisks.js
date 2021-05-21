4517c1e8b2c43140ab3bdbb3af4b2535a0306498
EddieRAM <edusacarneiro@gmail.com>
2021-05-21 17:54:45 -0300

    Exercicio 4.3.1 - 1_printAsterisks

---
+++ 4.3/1_printAsterisks.js
@@ -0,0 +1,11 @@
+// Criar array contendo uma sequência de * de qualquer tamanho > 1
+// Imprimir a array tantas vezes quanto o número de * da array
+
+let asterisks = ["*", "*", "*"]; 
+let arr = [];
+
+for (let index = 1; index < asterisks.length +1; index += 1) {
+  console.log(asterisks);
+}
+
+
---
+++ 4.3/Algo_ 2.js
@@ -0,0 +1,4 @@
+//1. Criar variável com valor 0 para receber a soma de todos os valores;
+//2. Criar um "for" para percorrer o array;
+//3. Adicionar os valores na variável criada;
+//4. Imprimir com o console.log a variável criada.
