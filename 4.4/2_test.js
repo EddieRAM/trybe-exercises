11c7da7d30b509f017950b9a14c0c6fb4c490811
EddieRAM <edusacarneiro@gmail.com>
2021-05-24 19:23:22 -0300

    Course 4.4 - tests

---
+++ 4.4/2_test.js
@@ -0,0 +1,5 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(index, cars[index]);
+}
---
+++ 4.4/7_carKeys.js
@@ -0,0 +1,10 @@
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let keys in car) {
+  console.table(car);
+  break
+}
