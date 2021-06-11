23d0512b6a1c023e37df64dd2af8439b39e319ed
EddieRAM <edusacarneiro@gmail.com>
2021-06-10 23:36:31 -0300

    Initial commit

---
+++ 6.2/script.js
@@ -0,0 +1,42 @@
+const state = document.getElementById('user-state');
+const allStates = [
+  ['Acre', 'AC'],
+  ['Alagoas', 'AL'],
+  ['Amapá', 'AP'],
+  ['Amazonas', 'AM'],
+  ['Bahia', 'BA'],
+  ['Ceará', 'CE'],
+  ['Distrito Federal', 'DF'],
+  ['Espírito Santo', 'ES'],
+  ['Goiás', 'GO'],
+  ['Maranhão', 'MA'],
+  ['Mato Grosso', 'MT'],
+  ['Mato Grosso do Sul', 'MS'],
+  ['Minas Gerais', 'MG'],
+  ['Pará', 'PA'],
+  ['Paraíba', 'PB'],
+  ['Paraná', 'PR'],
+  ['Pernambuco', 'PE'],
+  ['Piauí', 'PI'],
+  ['Rio de Janeiro', 'RJ'],
+  ['Rio Grande do Norte', 'RN'],
+  ['Rio Grande do Sul', 'RS'],
+  ['Rondônia', 'RO'],
+  ['Roraima', 'RR'],
+  ['Santa Catarina', 'SC'],
+  ['São Paulo', 'SP'],
+  ['Sergipe', 'SE'],
+  ['Tocantins', 'TO']
+];
+
+function createStates() {
+  for (let index = 0; index < allStates.length; index += 1) {
+    const anotherState = document.createElement('option');
+    anotherState.innerText = allStates[index][0];
+    anotherState.value = allStates[index][1];
+    state.appendChild(anotherState);
+  }
+}
+window.onload = function() {
+createStates();
+}
