ad088b54a7fe4c309ddddf70a5f638316bce44ae
EddieRAM <edusacarneiro@gmail.com>
2021-06-29 10:50:32 -0300

    Exercicio 3

---
+++ 8.4/exercise3.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+const expectedResult = 43;
+
+
+function averageAge() {
+  // escreva seu código aqui
+  // 1. Retornar a idade que os autores tinham quando os livros foram lançados em um array através do map();
+  const authorAgeArr = books.map((book) => book.releaseYear - book.author.birthYear);
+  // 2. Retornar o acumulador a ser utilizado como parâmetro no reduce, que pode ser o somatório de todas as idades;
+  const ageSum = authorAgeArr.reduce((age, currentValue) => age + currentValue);
+  // 3. Agora é só dividir pelo número de ítens do array.
+  const averageAge = ageSum / authorAgeArr.length;
+  return averageAge;
+}
+
+assert.strictEqual(averageAge(), expectedResult);
