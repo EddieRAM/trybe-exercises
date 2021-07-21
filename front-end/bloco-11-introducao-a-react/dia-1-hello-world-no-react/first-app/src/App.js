54b9ca4dcd650f5bfa51952b31cb4268ce29c16a
EddieRAM <edusacarneiro@gmail.com>
2021-07-21 12:31:57 -0300

    Some adjustments

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/first-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/first-app/src/App.js
@@ -1,21 +1,39 @@
     
-    import React from 'react';
-    import './App.css';
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
 
-    const Task = (value) => {
-      return (
-        <li key={value}>{value}</li>
-      );
-    }
+/* Crie uma lista de tarefas simples que:
+Utilize a função Task dentro do componente App .
+Leia as tarefas de dentro de um Array.
+Use a função map para criar cada item da lista no HTML. */
 
-    const tasks = ['Read', 'Study', 'Code', 'Work'];
+const task = () => {
+  const arrtasks = ['Read', 'Study', 'Code', 'Work'];
+  return arrtasks.map(item => <li>{item}</li>)
 
-    class App extends React.Component {
-      render() {
-        return (
-          <ul>{ tasks.map(task => Task(task)) }</ul>
-        );
-      }
-    }
+}    
 
-    export default App;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Theese are my tasks
+        </p>            
+        <ul>{task()}</ul>            
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
