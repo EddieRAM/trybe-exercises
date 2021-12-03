ba0bb7c812153286bc8192c189e45399a0665d6f
EddieRAM <edusacarneiro@gmail.com>
2021-12-03 14:36:17 -0300

    exercicios 26.3 1 mod

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercicios-26_3/numberSignTest.test.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/exercicios-26_3/numberSignTest.test.js
@@ -3,19 +3,19 @@ const { expect } = require('chai');
 const whichNumberSign = require('./numberSign');
 describe('if the function whichNumberSign is working', () => {
   describe('if the number is positive, negative or neuter' , () => {
-    describe('if the number is positive', () => {
+    describe('if the number is above 0', () => {
       it('must return POSITIVE', () => {
         const result = whichNumberSign(1);
         expect(result).to.be.equals('POSITIVE');
       });    
     });
-    describe('if the number is negative', () => {
+    describe('if the number is below 0', () => {
       it('must return NEGATIVE', () => {
         const result = whichNumberSign(-1);
         expect(result).to.be.equals('NEGATIVE');
       }); 
     });
-    describe('if the number is neuter', () => {
+    describe('if the number is 0', () => {
       it('must return NEUTER', () => {
         const result = whichNumberSign(0);
         expect(result).to.be.equals('NEUTER');
