273d8da5af1f96afaec5e182a36642d2034bde8a
EddieRAM <edusacarneiro@gmail.com>
2022-01-11 14:41:58 -0300

    exercicios 26.2 4 (3)

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-26_2/1_threeParamProm.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios-26_2/1_threeParamProm.js
@@ -8,22 +8,23 @@ Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise
 
  Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito
  baixo"
-Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.*/
+Caso o resultado seja maior que 50, resolva a Promise com o valor obtido. */
 
 function pamParam(num1, num2, num3) {
-  return new Promise((resolve,reject) => {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number')
-    
-    reject('Informe apenas números');
+  return new Promise((resolve, reject) => {
+    if (
+      typeof num1 !== 'number'
+      || typeof num2 !== 'number'
+      || typeof num3 !== 'number'
+    ) { reject(new Error('Informe apenas números')); }
 
     const result = (num1 + num2) * num3;
 
     if (result < 50) {
-      return reject(`Valor muito baixo`);
+      return reject(new Error('Valor muito baixo'));
     }
 
     resolve(result);
-
   });
 }
 
@@ -36,6 +37,8 @@ function pamParam(num1, num2, num3) {
 .catch(error => console.log(error));
  */
 
-pamParam(2, 2, 15)
-.then(resolve => console.log(resolve))
-.catch(error => console.log(error));
+pamParam(3, 2, 15)
+  .then((resolve) => console.log(resolve))
+  .catch((error) => console.log(error));
+
+module.exports = pamParam;
